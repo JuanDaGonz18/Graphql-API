ef4718114eb4bbe85c8d79e0fe725689b52c3142
julian barragan perez <julianbarpe@unisabana.edu.co>
2025-11-01 19:16:56 -0500

    Versión inicial con Apollo Client3

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,10 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// Configuración básica para desarrollo
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173
-  }
+  plugins: [react()]
 })
